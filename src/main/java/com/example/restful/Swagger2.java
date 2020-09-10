c470c19d28b00ae5f46ef3b7743e6cf3d219ec4a
basetotoro <631862061@qq.com>
2020-09-10 15:33:59 +0800

    使用Swagger2构造RESTful API文档

---
+++ src/main/java/com/example/restful/Swagger2.java
@@ -0,0 +1,37 @@
+package com.example.restful;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class Swagger2 {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.restful.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot中使用Swagger2构建RESTful APIs")
+                .description("原文地址链接：http://blog.didispace.com/springbootswagger2/")
+                .termsOfServiceUrl("http://blog.didispace.com/")
+                .contact(new Contact("@Totoro大大","http://blog.didispace.com/springbootswagger2/","631862061@qq.com"))
+                .version("1.0")
+                .build();
+    }
+}
